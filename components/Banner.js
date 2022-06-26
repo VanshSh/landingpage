382ba4b71b37a17454e4f68de461b30c9d04fd98
Vansh Sharma <vansh@nowandme.com>
2022-06-26 12:34:23 +0530

    style(Banner): :lipstick: Padding in the Image

--- components/Banner.js
+++ components/Banner.js
@@ -1,30 +1,29 @@
 import Image from 'next/image'
 import Header from '../public/header.png'
+import { BsApple } from 'react-icons/bs'
+import { AiFillAndroid } from 'react-icons/ai'
 const Banner = () => {
     return (
-        <header className='bg-red-50 lg:text-start text-center px-3  flex flex-1 lg:flex-row flex-col justify-center  lg:items-center'>
-            <div className='lg:w-fit w-full   mr-10 '>
-                <h1 className='text-[2.4em] text-red-900 font-extrabold '>
-                    A safe space for women <br /> to meet and find support
+        <header className='flex flex-1 lg:flex-row flex-col justify-start lg:justify-center lg:items-center pt-5 lg:px-24 px-5 '>
+            <div className='flex flex-col mt-24 justify-start max-w-3xl '>
+                <h1 className='text-5xl font-extrabold text-[#7f0924] '>
+                    A safe space for women to meet and find support
                 </h1>
-
-                <p className=' text-xl my-5'>
+                <p className=' mt-10 text-xl'>
                     Connect with women who are at a similar stage in life–from
-                    fertility, <br /> pregnancy and motherhood through to
-                    menopause.
+                    fertility, pregnancy and motherhood through to menopause.
                 </p>
-
-                <div className='flex flex-1 lg:justify-start justify-center gap-2'>
-                    <button className='bg-[#ff4f55] px-10 rounded  py-3 text-white font-bold'>
-                        iOS
+                <div className='flex w-fit justify-between mt-8 '>
+                    <button className=' px-14 py-2 text-4xl rounded-lg inline-flex bg-[#fe5055] text-white'>
+                        <BsApple />
                     </button>
-                    <button className='bg-[#ff4f55] px-10 rounded  py-3 text-white font-bold'>
-                        Android
+                    <button className=' px-14 py-2 text-4xl lg:ml-10 ml-4 rounded-lg inline-flex bg-[#fe5055] text-white'>
+                        <AiFillAndroid />
                     </button>
                 </div>
             </div>
-            <div className='my-10 '>
-                <Image src={Header} height={450} width={400} />
+            <div className='mt-12 lg:ml-12 pb-14'>
+                <Image src={Header} height={550} width={473} />
             </div>
         </header>
     )
