45f0300140e9ff835f1dd7fcc9d5ebf798e20c02
Vansh Sharma <vansh@nowandme.com>
2022-07-06 16:31:24 +0530

    style: :lipstick: Button gap in the footer

--- src/components/CommunityType.js
+++ src/components/CommunityType.js
@@ -4,11 +4,11 @@ import Link from 'next/link'
 const CommunityType = () => {
     return (
         <section className='my-16 lg:mx-52 md:mx-16 mx-5'>
-            <p className='text-[#191919] text-center font-bold lg:text-[40px] md:text-[30px] text-[25px]  my-10 px-1 py-10'>
+            <p className='text-[#191919] text-center font-bold lg:text-[40px] md:text-[30px] text-[25px]  my-10 px-1 py-4'>
                 Now&Me provides access to a kind, empathetic and supportive
                 community of compassionate people
             </p>
-            <div className='mt-16 mb-10 flex w-full justify-evenly font-normal items-center  flex-wrap '>
+            <div className='mt-12 mb-10 flex w-full justify-evenly font-normal items-center  flex-wrap '>
                 <div className=' text-center text-xl p-2'>
                     <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
                         <span>🧠</span>
