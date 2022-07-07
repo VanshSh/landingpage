3a155894f10248cbc03c0e5b52601a9fb50e2bb3
Vansh Sharma <vansh@nowandme.com>
2022-07-07 13:55:01 +0530

    style: :lipstick: Fixed the padding for the emojis

--- src/components/community-type.js
+++ src/components/community-type.js
@@ -8,13 +8,52 @@ const CommunityType = () => {
                 Now&Me is where people come together to have the most authentic
                 and meaningful conversations
             </p>
-            <div className='mt-12 mb-10 flex w-full justify-evenly font-normal items-center  flex-wrap '>
-                <div>
-                    <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
-                        <span>🧑‍💻</span>
+            <div className='mt-12 mb-10 flex w-full justify-evenly font-normal items-center flex-wrap '>
+                <div className='mt-12 mb-10 flex w-full justify-evenly font-normal items-center  flex-wrap '>
+                    <div className='flex flex-col items-center'>
+                        <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 w-fit '>
+                            <span>🧑‍💻</span>
+                        </div>
+                        <div className='text-center text-xl'>Career help</div>
                     </div>
-                    <div className='text-center text-xl'>Career help</div>
+                    <div className='flex flex-col items-center'>
+                        <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 w-fit '>
+                            <span>🧑‍💻</span>
+                        </div>
+                        <div className='text-center text-xl'>Fitness plans</div>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 w-fit '>
+                            <span>🧑‍💻</span>
+                        </div>
+                        <div className='text-center text-xl'>
+                            One-sided love
+                        </div>
+                    </div>
+                    <div className='flex flex-col items-center'>
+                        <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 w-fit '>
+                            <span>🧑‍💻</span>
+                        </div>
+                        <div className='text-center text-xl'>
+                            Deeply emotional things
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </section>
+    )
+}
+
+export default CommunityType
+
+{
+    /* <div className='mt-12 mb-10 flex w-full justify-evenly font-normal items-center  flex-wrap '>
+<div>
+    <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
+        <span>🧑‍💻</span>
+    </div>
+    <div className='text-center text-xl'>Career help</div>
+</div>
                 <div>
                     <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
                         <span>🧑 </span>
@@ -37,28 +76,5 @@ const CommunityType = () => {
                 <div className='text-center text-xl'>
                     Deeply emotional things
                 </div>
-            </div>
-        </section>
-    )
+            </div> */
 }
-
-export default CommunityType
-
-//  <div className=' text-center text-xl p-2'>
-//                     <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
-//                         <span>🧠</span>
-//                     </div>
-//                     Fitness plans
-//                 </div>
-//                 <div className=' text-center text-xl p-2'>
-//                     <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
-//                         <span>😔</span>
-//                     </div>
-//                     One-sided love
-//                 </div>
-//                 <div className=' text-center text-xl p-2'>
-//                     <div className='bg-[#fff6f3] p-10 text-4xl rounded-full mb-5 '>
-//                         <span>🧡</span>
-//                     </div>
-//                     Deeply emotional things
-//                 </div>
