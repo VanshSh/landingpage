5c7bbdbbdfcdf4b26872848b68bd90814d2b1a3c
Vansh Sharma <vansh@nowandme.com>
2022-07-07 13:40:25 +0530

    fix: :pencil2:

--- src/components/news.js
+++ src/components/news.js
@@ -50,7 +50,7 @@ const NewsReview = () => {
                 >
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
-                        className='h-7'
+                        className='h-8'
                         viewBox='0 0 114 40'
                     >
                         <g fill='none' fillRule='evenodd'>
