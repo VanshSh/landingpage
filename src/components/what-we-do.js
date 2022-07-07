acf3fa17e1d031397e7c6a7fdfde530f0ed36a6e
Vansh Sharma <vansh@nowandme.com>
2022-07-07 13:32:10 +0530

    refactor: :pencil2:

--- src/components/what-we-do.js
+++ src/components/what-we-do.js
@@ -9,10 +9,11 @@ const WhatWeDo = () => {
             <div className='grid lg:grid-cols-2 grid-rows-2 justify-center'>
                 <section className=' px-2 py-10 text-center'>
                     <h2 className='text-center text-[#ff6d42] m-auto w-fit px-4 font-bold mb-3 text-6xl mt-12  '>
-                        Share
+                        Connections
                     </h2>
                     <p className='mt-4 lg:text-xl w-fit m-auto lg:w-2/3 text-center mb-[50px] '>
-                        Voice your thoughts with like-minded people.
+                        Beyond chatting, build personal connections with people
+                        who understand and care.
                     </p>
                     <div className='w-[280px] height-[400px]  text-center my-0 mx-auto'>
                         <Image src={Share} alt='Share' />
@@ -20,11 +21,11 @@ const WhatWeDo = () => {
                 </section>
                 <section className=' px-2 py-10 text-center'>
                     <h2 className='text-center text-[#ff6d42] m-auto w-fit px-4 font-bold mb-3 text-6xl mt-12  '>
-                        Comment
+                        Anonymous postings
                     </h2>
                     <p className='mt-4 lg:text-xl w-fit m-auto lg:w-2/3 text-center mb-9 '>
-                        Discover and chat with people who understand what
-                        you&apos;re going through.
+                        So you can do YOU. Not your job, the number of followers
+                        or social status, define who you are
                     </p>
                     <div className='w-[280px] height-[400px] text-center my-0 mx-auto'>
                         <Image src={Comment} alt='Comments' />
@@ -32,10 +33,12 @@ const WhatWeDo = () => {
                 </section>
                 <section className=' px-2 py-10 text-center'>
                     <h2 className='text-center text-[#ff6d42] m-auto w-fit px-4 font-bold mb-3 text-6xl mt-12  '>
-                        Groups
+                        Discussions
                     </h2>
                     <p className='mt-4 lg:text-xl w-fit m-auto lg:w-2/3 text-center mb-9 '>
-                        Make groups and talk about your special interests.
+                        The real action is always in the comments! Comforting
+                        words, anecdotes from lived experiences & the feeling of
+                        solidarity.
                     </p>
                     <div className='w-[280px] height-[400px] text-center my-0 mx-auto'>
                         <Image src={Group} alt='Groups' />
@@ -43,11 +46,11 @@ const WhatWeDo = () => {
                 </section>
                 <section className=' px-2 py-10 text-center'>
                     <h2 className='text-center text-[#ff6d42] m-auto w-fit px-4 font-bold mb-3 text-6xl mt-12  '>
-                        Events
+                        Groups
                     </h2>
                     <p className='mt-4 lg:text-xl w-fit m-auto lg:w-2/3 text-center mb-9 '>
-                        Take part in our Sharing Circles and other community
-                        events.
+                        Create your community of like-minded friends to talk
+                        about your special interests.
                     </p>
                     <div className='w-[280px] height-[400px] text-center my-0 mx-auto'>
                         <Image src={Group} alt='Events' />
